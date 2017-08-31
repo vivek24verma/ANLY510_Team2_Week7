dda4480f65be8b8426e01e12e7930360311683e8
mvishniakova <mvishniakova@my.harrisburgu.edu>
2017-08-30 23:09:26 -0400

    Mariia: I filled in the missing values and dropped the HTML Column

---
+++ Team2_Week7_Data_CleanUp.R
@@ -0,0 +1,11 @@
+dirty <- read.csv("/Users/mariia/Desktop/anly510/dirty_data.csv", header=TRUE, na.strings=c("","NA"))
+names(dirty)
+install.packages("zoo")
+require(zoo)
+install.packages("data.table")
+library(data.table)
+dirty_no_missing = as.data.table(dirty)
+setDT(dirty_no_missing)[,Area := na.locf(na.locf(Area, na.rm=FALSE), fromLast=TRUE) , by = Year]
+dirty_no_missing
+dirty_no_missing_no_col <-dirty_no_missing
+dirty_no_missing_no_col$Strange.HTML <- NULL
